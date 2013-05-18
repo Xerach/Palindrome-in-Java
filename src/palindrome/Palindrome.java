1a2d7b19d36ab91142dacee6a00a118aa2c5ace7
Xerach <Xerach@10.230.175.226>
2013-05-18 11:04:59 +0100

    First Commit. Create test and isPalindrome method.

---
+++ src/palindrome/Palindrome.java
@@ -0,0 +1,12 @@
+package palindrome;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static Boolean isPalindrome(Integer number){        
+        return true;
+    }
+}
---
+++ test/palindrome/PalindromeTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palindrome;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class PalindromeTest {
+    
+    public PalindromeTest() {
+    }
+    
+    
+    @Test
+    public void testMain() {
+        assertTrue(Palindrome.isPalindrome(1234321));
+
+    }
+}
