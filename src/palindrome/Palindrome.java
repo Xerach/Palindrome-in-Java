6d4910e8326eb1187219e7df7acf6096fd613ab6
Xerach <Xerach@10.230.175.226>
2013-05-18 11:57:19 +0100

    Created new Tests: +DecimalisPalindrome +OctalisPalindrome +HexadecimalisPalindrome +BinaryisPalindrome

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -13,4 +13,5 @@ public class Palindrome {
         }
         return true;
     }
+    
 }
--- test/palindrome/PalindromeTest.java
+++ test/palindrome/PalindromeTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package palindrome;
 
 import org.junit.Test;
@@ -12,14 +8,31 @@ public class PalindromeTest {
     
     public PalindromeTest() {
     }
-    
+        
+    @Test
+    public void DecimalPalindrome() {
+        assertTrue(Palindrome.isPalindrome(1234321,10));
+        assertTrue(Palindrome.isPalindrome(9,10));
+        assertFalse(Palindrome.isPalindrome(785,10));
+        assertFalse(Palindrome.isPalindrome(1210,10));
+        assertFalse(Palindrome.isPalindrome(01210,10));
+    }
     
     @Test
-    public void testMain() {
-        assertTrue(Palindrome.isPalindrome(1234321));
-        assertTrue(Palindrome.isPalindrome(9));
-        assertFalse(Palindrome.isPalindrome(785));
-        assertFalse(Palindrome.isPalindrome(1210));
-        assertFalse(Palindrome.isPalindrome(01210));
+    public void OctalPalindrome() {
+        assertFalse(Palindrome.isPalindrome(273,8));
+    }
+    
+    @Test
+    public void HexadecimalPalindrome() {
+        assertFalse(Palindrome.isPalindrome(1234321,16));
+        assertTrue(Palindrome.isPalindrome(289,16));
+        assertTrue(Palindrome.isPalindrome(272,16));
+    }
+    
+    @Test
+    public void BinaryPalindrome() {
+        assertFalse(Palindrome.isPalindrome(1234,2));
+        assertTrue(Palindrome.isPalindrome(273,2));
     }
 }
