bcc1f1471ed9fe999ea60859944b2afb56db5598
Xerach <Xerach@10.230.175.226>
2013-05-18 11:07:04 +0100

    Created Test to isPalindrome

--- test/palindrome/PalindromeTest.java
+++ test/palindrome/PalindromeTest.java
@@ -17,6 +17,9 @@ public class PalindromeTest {
     @Test
     public void testMain() {
         assertTrue(Palindrome.isPalindrome(1234321));
-
+        assertTrue(Palindrome.isPalindrome(9));
+        assertFalse(Palindrome.isPalindrome(785));
+        assertFalse(Palindrome.isPalindrome(1210));
+        assertFalse(Palindrome.isPalindrome(01210));
     }
 }
